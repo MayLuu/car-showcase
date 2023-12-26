82a642e1a6e74b777c6e2e48458c1d3dae5f47f9
MayLuu <myluu4444@gmail.com>
2023-12-26 18:47:18 +0700

    Fix card link and add data done

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,14 +1,16 @@
+import React from "react";
+
 export const Card = ({ item }) => {
 
     var link = item.category.replace(' ', '-') + '/' + item.name.replace(' ', '-')
     return (
-        <>
+        <a href={link}>
             <img src={item.imageUrl[0]} alt="" />
             <h3>{item.name}</h3>
-            <span>{item.price} đ</span>
+            <h5>{item.price} đ</h5>
             <i className="bx bxs-star">(6 Đánh giá)</i>
-            <a href="/" className="btn" style={{ width: "100%", padding: "6px 8px" }}><i class='bx bx-cart-add' style={{ color: "white" }}></i>  Mua ngay</a>
+            <a href={link} className="btn" style={{ width: "100%", padding: "6px 8px" }}><i class='bx bx-cart-add' style={{ color: "white" }}></i>Mua ngay</a>
             <a href={link} className="details">Xem chi tiết</a>
-        </>
+        </a>
     );
 }
--- src/pages/AllList.js
+++ src/pages/AllList.js
@@ -13,7 +13,7 @@ export const AllList = () => {
                     <h1>Các dòng xe Mercedes-Benz </h1>
                     <span className="bold">Những dòng xe sang trọng và đẳng cấp</span>
                     <p style={{}}>
-                        Khách hàng liên hệ trực tiếp với MErcedes sẽ nhận ưu đãi tiền mặt đến hàng trăm triệu đồng cùng gói quà tặng bảo hiểm. phụ kiện cao cấp.
+                        Khách hàng liên hệ trực tiếp với Mercedes sẽ nhận ưu đãi tiền mặt đến hàng trăm triệu đồng cùng gói quà tặng bảo hiểm, phụ kiện cao cấp.
                     </p>
                 </div>
                 <div className="parts-container container ">
